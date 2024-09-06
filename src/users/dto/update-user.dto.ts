93d208c12670bfe6035d33451475aeb64804f507
Pedro Henrique Andriotti Bastos <phab@cesar.school>
2024-09-06 10:44:04 -0300

    consertando deploy railway

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { Role } from '@prisma/client';
+import { IsEnum } from '@nestjs/class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) { }
+
+
+export class UpdateUserRoleDto extends PartialType(CreateUserDto) {
+  @IsEnum(Role)
+  role: Role;
+}
